189ae7c87ed85ffc28ee1eef5d7913069f197ab7
Mykhaylo Sorochan <msorochan@sphereconsultinginc.com>
2010-10-20 10:53:08 +0300

    Rails version, README, database.yml

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -32,9 +32,9 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-  config.active_record.observers = :acceptance_criterion_observer, 
-                                   :user_observer, 
-                                   :organisation_member_observer, 
+  config.active_record.observers = :acceptance_criterion_observer,
+                                   :user_observer,
+                                   :organisation_member_observer,
                                    :story_action_observer,
                                    :organisation_observer,
                                    :repeat_observer,
@@ -48,5 +48,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-  
+
 ExceptionNotifier.exception_recipients = %w{admin@jandaweb.com}
